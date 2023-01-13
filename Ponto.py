c805443ab20b16112dcc7c8404d179ce675747f9
AndreRistow <91630769+AndreRistow@users.noreply.github.com>
2023-01-13 10:07:30 -0300

    Código da automação

---
+++ Ponto.py
@@ -0,0 +1,28 @@
+#Automatização para bater o ponto
+#Importar biblotecas necessarias
+import pyautogui
+import time
+#1 seg de pausa a cada comando
+pyautogui.PAUSE = 1
+#Abrir site do ponto
+pyautogui.press(["Win"])
+pyautogui.write("chrome")
+pyautogui.press(["enter"])
+time.sleep(1)
+pyautogui.write("http://meurh.iesb.br:8081/01/#/login")
+pyautogui.press(["enter"])
+time.sleep(1)
+#Colocar login
+pyautogui.write("***********")
+pyautogui.press(["Tab"])
+#Colocar senha
+pyautogui.write("******")
+time.sleep(1)
+pyautogui.press(["enter"])
+time.sleep(6)
+#Trafegar pelo site e procurar opção "bater ponto"
+pyautogui.doubleClick("botao2.PNG")
+time.sleep(6)
+#Bater o ponto
+pyautogui.doubleClick("botaox.PNG")
+pyautogui.dragTo(100,200,2, button="left")
